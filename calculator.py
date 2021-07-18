51bfbde1ac3a225b17d1d6f2dd7c976f76b95b69
jtamayga <87560179+jtamayga@users.noreply.github.com>
2021-07-18 00:59:27 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -19,7 +19,7 @@ def divide(x, y):
     """Divide Function"""
     if y == 0:
         raise ValueError('Can not divide by zero!')
-    return x / y
+    return float(x / y)
 
 class TestCalc(unittest.TestCase):
 
