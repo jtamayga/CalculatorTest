41d4c448e3f03b569a4f3d846779511a4774f70d
jtamayga <87560179+jtamayga@users.noreply.github.com>
2021-07-18 00:54:46 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import unittest
+
 def add(x, y):
     """Add Function"""
     return x + y
@@ -18,3 +20,33 @@ def divide(x, y):
     if y == 0:
         raise ValueError('Can not divide by zero!')
     return x / y
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 5), 50)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, -1), 1)
+        self.assertEqual(divide(5, 2), 2.5)
+
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
